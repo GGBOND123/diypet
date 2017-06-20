871a0b78399115688c2646475f0f0fdcc8a9f989
Tristan Damron <TristanDamron31@gmail.com>
2017-06-20 12:56:24 -0700

    Added tentacles and wrote script for making them turn into rag dolls from animationed objects

---
+++ Assets/Scripts/GoRagDoll.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoRagDoll : MonoBehaviour {
+
+    private Vector3 pos_;
+    private float timer = 0f;
+    public List<GameObject> ragdolls;
+
+    void Update() {
+        if (timer >= 1f)
+        {
+            this.pos_ = transform.position;
+            this.timer = 0f;
+        } else
+        {
+            if (transform.position != this.pos_)
+            {
+                foreach (GameObject g in ragdolls)
+                {
+                    g.GetComponent<Animator>().enabled = false;
+                }
+            } else
+            {
+                foreach (GameObject g in ragdolls)
+                {
+                    g.GetComponent<Animator>().enabled = true;
+                }
+            }
+            this.timer += Time.deltaTime;
+        }	
+	}
+}
