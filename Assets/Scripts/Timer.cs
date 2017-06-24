1e4a1334a0f2b686b6d0543ac47d0394bdfa391f
Tristan Damron <TristanDamron31@gmail.com>
2017-06-24 12:39:28 -0700

    Updated clock

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -8,9 +8,10 @@ public class Timer : MonoBehaviour {
 
 	void Update () {
         this.timer += Time.deltaTime;
-        if (this.timer >= 1f)
+        if (this.timer >= 1f && this.cutoff > .09f)
         {
-            this.cutoff -= 0.0033f;
+            //this.cutoff -= 0.0033f;
+            this.cutoff -= 0.05f;
             GetComponent<Renderer>().material.SetFloat("_Cutoff", this.cutoff);
             this.timer = 0f;
         }
