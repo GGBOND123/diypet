343baa0cee628e9ab60a634514aea62b0c259176
Seanna M <evilseanbot@gmail.com>
2017-06-24 16:16:25 -0700

    Modifications of materials, addition of box

---
+++ Assets/diypet/Box/DissappearWhenClose.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace diypet
+{
+    public class DissappearWhenClose : MonoBehaviour
+    {
+        public GameObject head;
+        private bool wasClose = false;
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Vector3.Distance(head.transform.position, gameObject.transform.position) < 0.75f)
+            {
+                wasClose = true;
+            }            
+
+            if (wasClose)
+            {
+                transform.Translate(Vector3.forward * 0.1f);
+            }
+        }
+    }
+}
