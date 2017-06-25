f1e5c606bd759e50bf77a64e48dba6ac6c20c4ce
Tristan Damron <TristanDamron31@gmail.com>
2017-06-25 09:43:17 -0700

    Created a kibble prefab

---
+++ Assets/diypet/Pet/Kibble.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace diypet
+{
+    public class Kibble : MonoBehaviour
+    {
+        void OnTriggerEnter(Collider c)
+        {
+            if (c.tag == "Pet")
+            {
+                c.GetComponent<PetBehavior>().hungryNeed.currentNeedLevel = 0;
+                Destroy(gameObject);
+            }
+        }
+    }
+
+}
