0f57e8ac846d13da9c2c443a7b6153ec1768daac
Tristan Damron <TristanDamron31@gmail.com>
2017-06-25 12:50:50 -0700

    Added events into the game! House floods a bit, and a bird flys around the pet

--- Assets/diypet/Timer/Timer.cs
+++ Assets/diypet/Timer/Timer.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 namespace diypet {
     public class Timer : MonoBehaviour {
 
-        private bool started = false;
+        private bool started = true;
         public float runningTime;
+        public GameObject bird;
+        public GameObject flood;
+
+        private bool bird_spawn = false;
+        private bool flood_spawn = false;
 
 	    // Use this for initialization
 	    void Start () {
@@ -19,11 +24,31 @@ namespace diypet {
             {
                 runningTime += Time.deltaTime;
             }
-	    }
+
+            if (runningTime >= 120 && !this.bird_spawn)
+            {
+                this.bird.GetComponent<BirdController>().enabled = true;
+            }
+
+            if (runningTime >= 200 && !this.flood_spawn)
+            {
+                RaiseFlood();    
+            }
+        }
 
         public void TurnOn()
         {
             started = true;
+        }
+
+        public void RaiseFlood()
+        {
+            this.flood.transform.position = new Vector3(this.flood.transform.position.x, this.flood.transform.position.y + 0.25f, this.flood.transform.position.z);
+
+            if (this.flood.transform.position.y >= -33f)
+            {
+                this.flood_spawn = true;
+            } 
         }
     }
 }
