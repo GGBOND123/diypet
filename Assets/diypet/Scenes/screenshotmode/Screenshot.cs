d8cf2e6c89e416330bd69aa39732b6a8c97b93d8
Thomas Newlands <thomas@newlands.cc>
2017-07-03 17:57:47 -0700

    screenshot branch

---
+++ Assets/diypet/Scenes/screenshotmode/Screenshot.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Screenshot : MonoBehaviour
+{
+    public int superSize = 2;
+    public string filename = "screenshot";
+
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            Application.CaptureScreenshot(filename + ".png", superSize);
+            print("screenshot captured");
+        }
+    }
+}
